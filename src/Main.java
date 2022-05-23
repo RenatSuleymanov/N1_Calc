10e7b8b40f71ccea3b691d0b85772a9c107323b9
RenatSuleymanov <Renat.Suleymanov.job@users.noreply.github.com>
2022-05-24 00:18:19 +0500

    test1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        // Ввод первого числа
+        System.out.print("Введите первое число и нажмите Enter: ");
+        double firstNumber = scan.nextDouble();
+        // Ввод второго числа
+        System.out.print("Введите второе число и нажмите Enter: ");
+        double secondNumber = scan.nextDouble();
+        // Суммирование двух чисел и вывод в консоль
+        double sum = firstNumber + secondNumber;
+        System.out.printf("Сумма: %.4f", sum);
+        scan.close();
     }
 }
